9728fe8fefea16b9270751dba6b92e5391b5b5c7
Ioana Medvesan <ioanamedvesan@yahoo.com>
2020-01-27 18:36:38 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -23,7 +23,7 @@ if (13 <=age && age <= 19) {
 }
 
 function ternaryTeenager(age) {
-
+return 13 <= age && age <= 19 ? "You are a teenager" : "You are not a teenager";
 }
 
 function switchAge(age) {
