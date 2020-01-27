6156e7b36f2360d5ff1389e5dfe9e717afc9d24d
Ioana Medvesan <ioanamedvesan@yahoo.com>
2020-01-27 18:32:13 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -5,7 +5,7 @@ if (13 <= age && age <= 19) {
 }
 
 function teenager(age) {
-if (13 <= age && age<= 19) {
+if (13 <= age && age <= 19) {
   return "You are a teenager!";
 } else {
   return "You are not a teenager";
@@ -13,7 +13,13 @@ if (13 <= age && age<= 19) {
 }
 
 function ageChecker(age) {
-
+if (13 <=age && age <= 19) {
+  return "You are a teenager!";
+} else if (age <= 12) {
+  return "You are a kid";
+} else {
+  return "You are a grownup";
+}
 }
 
 function ternaryTeenager(age) {
